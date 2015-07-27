13457484dc053d1ef56b837d6aa7cc5e2fe543dc
Yu Wang <wangy675@163.com>
2015-07-27 07:12:43 -0400

    create database

---
+++ sql/sqla.py
@@ -0,0 +1,13 @@
+# Create a SQLite3 database and table
+
+import sqlite3
+
+conn = sqlite3.connect("new.db")
+
+cursor = conn.cursor()
+
+cursor.execute("""CREATE TABLE population(
+    city TEXT, state TEXT, population INT)
+    """)
+
+conn.close
