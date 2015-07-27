f10db389099fb7b8eb9ac0348202a289b9294f2c
Yu Wang <wangy675@163.com>
2015-07-27 04:54:39 -0400

    flask-hello-world-first

---
+++ app.py
@@ -0,0 +1,14 @@
+# --- Flask Hello Word ---
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/hello")
+
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    app.run()
