ba57cfe0567839e433ef993f8130c9f37e458c8f
Yu Wang <wangy675@163.com>
2015-07-29 01:51:51 -0400

    improve according to book

--- sql/assignment3b.py
+++ sql/assignment3b.py
@@ -8,20 +8,28 @@ sql = {"average":"SELECT AVG(num) FROM numbers",
     "minimum":"SELECT MIN(num) FROM numbers",
     "sum":"SELECT SUM(num) FROM numbers"
     }
-    
+
+prompt="""
+Select the operation that you want to perform [1-5]:
+1. Average
+2. Max
+3. Min
+4. Sum
+5. Exit
+"""
 while True:    
     try:
-        your_input = raw_input("Choices:\n1:do average\n2:do max\n3:do min\n4:do sum\nother:exit\nPlease input your select:")
-        choice = int(your_input)
-        if choice not in range(1,5):
+        your_input = raw_input(prompt)
+        if int(your_input) not in range(1,5):
             sys.exit()
+        operation = {"1": "avg", "2":"max", "3":"min","4":"sum"}[your_input]
         with sqlite3.connect("newnum.db") as connection:
             c = connection.cursor()
             # print sql.values()[choice-1]
-            c.execute(sql.values()[choice-1])
+            c.execute("SELECT {}(num) FROM numbers".format(operation))
             result = c.fetchone()
 
-            print sql.keys()[choice-1], result[0]
+            print operation + ": %f" % result[0]
     except Exception, e:
         print e
         sys.exit()
