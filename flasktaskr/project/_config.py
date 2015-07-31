ac9f81cee5881796a603e5b4ba27fa6dc79ff4bc
Yu Wang <wangy675@163.com>
2015-07-31 03:47:51 -0400

    flask-taskr using SQLAlchemy

--- flasktaskr/project/_config.py
+++ flasktaskr/project/_config.py
@@ -5,7 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DATABASE = 'flasktaskr.db'
 USERNAME = 'admin'
 PASSWORD = 'admin'
-WTF_CSRF_ENABLED = True
+CSRF_ENABLED = True
 SECRET_KEY = "\x99u\xb3\xd8\xe2\\\x95\xf6\x95\x01vv\x04\x88'Ohq\xba_\x00\xb5\x83\x90"
 
 DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_PATH
