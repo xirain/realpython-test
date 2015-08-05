fba69db751f456fb543de0c02262ba473f7907ae
Yu Wang <wangy675@163.com>
2015-08-05 01:47:42 -0400

    little

--- flasktaskr/project/__init__.py
+++ flasktaskr/project/__init__.py
@@ -1,8 +1,9 @@
 # project/__init__.py
 
-from flask import Flask, render_template, request, Blueprint
+from flask import Flask, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.bcrypt import Bcrypt
+import datetime
 
 #####################
 ###### config #######
@@ -20,8 +21,21 @@ app.register_blueprint(tasks_blueprint)
 
 @app.errorhandler(404)
 def not_found(error):
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a+') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write("\n 404 error at {}: {}".format(current_timestamp, r))
     return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_error(error):
+    db.session.rollback()
+    if app.debug is not True:
+        now = datetime.datetime.now()
+        r = request.url
+        with open('error.log', 'a+') as f:
+            current_timestamp = now.strftime("%d-%m-%Y %H:%M:%S")
+            f.write("\n 500 error at {}: {}".format(current_timestamp, r))
     return render_template('500.html'), 500
